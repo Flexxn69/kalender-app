ce6d04816973f13510ba6a991cc726ea7a5a7f38
Flexxn69 <flexxtekkx@gmail.com>
2025-06-27 17:11:37 +0200

    Fix: Entferne dynamic ssr:false für Next.js 15

--- app/messages/[conversationId]/page.tsx
+++ app/messages/[conversationId]/page.tsx
@@ -1,7 +1,7 @@
-import dynamic from "next/dynamic"
-const MessagesPage = dynamic(() => import("../page"), { ssr: false })
+"use client"
+import MessagesPage from "../page"
 
 export default function MobileChatPage({ params }: { params: { conversationId: string } }) {
   // Immer mobile Ansicht erzwingen, unabhängig von window.innerWidth
-  return <MessagesPage mobileConversationId={params.conversationId} forceMobile={true} />
+  return <MessagesPage mobileConversationId={params.conversationId} forceMobile />
 }
